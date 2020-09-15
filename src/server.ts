4f1b4c9e9cc0502ec6fcf5e6c08948b596e7655a
Hardik Singh <hardiksingh297@gmail.com>
2020-09-15 19:10:54 +0530

    Setup Express server

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import express = require("express");
+import router from "./routes/router";
+
+const PORT: number | string = process.env.PORT || 8000;
 
 const app: express.Application = express();
 
-app.get("/", (req, res): void => {
-  res.send("Working");
-});
+app.use(router);
 
-app.listen(3000, () => {
-  console.log("Listening on 3000");
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
 });
