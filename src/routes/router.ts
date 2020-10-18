ded840556e02347966fe32a94f0d2aafa9155aa5
Hardik Singh <hardiksingh297@gmail.com>
2020-10-18 10:26:08 +0530

    Added Enpoint Comments

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,6 +1,15 @@
 import express = require("express");
 const router: express.Router = express.Router();
 
+/* 
+BASIC ENDPOINTS ARE:
+    1. HOME
+    2. /blocks FOR VIEWING ALL THE BLOCKS
+    3. /blocks/mine  FOR MINING A BLOCK
+    4. /peers FOR VIEWING PEERS
+    5. /peers/add FOR ADDING
+*/
+
 router.get("/", (req, res) => {
   res.send("Home here");
 });
